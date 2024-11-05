d5246f2b2c0aca19dea6a1ff8fffdb972d13087a
Arghya Mondal <arghyamondal.work@gmail.com>
2024-11-05 08:12:30 +0000

    login ui built

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -1,9 +1,54 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
-
-export default function LoginPage({}) {
+export default function Page() {
   return (
-    <div>
-      LoginPage
+    <div className="flex h-screen w-full flex-col items-center justify-center px-4">
+      <img 
+        src="/assets/Logo.png" 
+        alt="Logo" 
+        className="mb-6 h-24 w-24" // Adjust size as needed
+      />
+      <div className="w-full max-w-md p-6 rounded-md">
+        <h1 className="text-4xl font-semibold text-center mb-2">Login to Continue</h1>
+        <p className="text-center text-muted-foreground text-sm mb-12">
+          Enter your email below to login to your account
+        </p>
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">Password</Label>
+              <Link
+                href="#"
+                className="ml-auto inline-block text-sm underline"
+              >
+                Forgot your password?
+              </Link>
+            </div>
+            <Input id="password" type="password" required />
+          </div>
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+        </div>
+        <div className="mt-4 text-center text-sm">
+          Don&apos;t have an account?{" "}
+          <Link href="#" className="underline hover:text-primary">
+            Sign up
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
